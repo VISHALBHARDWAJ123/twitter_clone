68d4b6c2ea0f76f75d6a56b9f94564f2a690a3e4
VISHALBHARDWAJ123 <vishal.matrid2380@gmail.com>
2022-08-03 16:20:11 +0530

    changes in login screen(near responsive)

--- lib/layout/pages/authentication/login.dart
+++ lib/layout/pages/authentication/login.dart
@@ -38,141 +38,154 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
+    return SafeArea(
+      bottom: true,
+      // top: true,
+      maintainBottomViewPadding: true,
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: Colors.white,
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.close_rounded,
-            color: Colors.black,
-            size: size.width * .085,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.close_rounded,
+              color: Colors.black,
+              size: size.width * .088,
+            ),
           ),
+          title: SizedBox(
+            height: size.height * .095,
+            width: size.width * .095,
+            child: Image.asset("assets/twitter_logo.png"),
+          ),
+          centerTitle: true,
         ),
-        title: SizedBox(
-          height: size.height * .095,
-          width: size.width * .095,
-          child: Image.asset("assets/twitter_logo.png"),
-        ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        physics: const NeverScrollableScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * .04,
-              ),
-              const Center(
-                child: AutoSizeText(
-                  'To get started, first enter your phone, email address or @username',
-                  maxLines: 3,
-                  style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(
-                height: size.height * .02,
-              ),
-              Center(
-                child: SizedBox(
-                  height: size.height * .09,
-                  width: size.width * .9,
-                  child: TextFormField(
-                    focusNode: focusNode,
-                    controller: _controller,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                    ),
-                    decoration: InputDecoration(
-                      focusColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(6.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            const BorderSide(color: Colors.blue, width: 1.0),
-                        borderRadius: BorderRadius.circular(6.0),
-                      ),
-                      fillColor: Colors.grey,
-                      labelText: 'Phone, email address, or username',
-                      labelStyle: const TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      floatingLabelStyle: const TextStyle(
-                        color: Colors.blueAccent,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: size.height * .41,
-              ),
-              const Divider(
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: size.height * .02,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: size.width * .02,
-                  ),
-                  SizedBox(
-                    height: size.height * .05,
-                    width: size.width * .4,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(width: 1)),
-                      child: const Center(
-                          child: Text(
-                        "Forgot Password?",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )),
-                    ),
-                  ),
-                  SizedBox(
-                    width: size.width * .3,
-                  ),
-                  SizedBox(
-                    height: size.height * .05,
-                    width: size.width * .2,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.black54,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(width: 1)),
-                      child: const Center(
-                          child: Text(
-                        "Next",
-                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white54),
-                      )),
-                    ),
-                  ),
-                ],
-              )
-            ],
+        body: SizedBox(
+          width: size.width,
+          height: size.height,
+          // padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(
+              children: [
+                _topPortion(size),
+                _forgotButton(size),
+                _nextButton(size),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Align _nextButton(Size size) {
+    return Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: size.height * .05,
+                    width: size.width * .2,
+                    decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(width: 1)),
+                    child: const Center(
+                        child: Text(
+                      "Next",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white54),
+                    )),
+                  ),
+                ),
+              );
+  }
+
+  Align _forgotButton(Size size) {
+    return Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: size.height * .05,
+                    width: size.width * .4,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(width: 1)),
+                    child: const Center(
+                        child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                ),
+              );
+  }
+
+  Positioned _topPortion(Size size) {
+    return Positioned(
+                top: size.height * .01,
+                right: 2,
+                left: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    AutoSizeText(
+                      'To get started, first enter your phone,email address or @username',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: size.width * 0.066,
+                          fontWeight: FontWeight.w800,
+                          letterSpacing: 2,
+                          wordSpacing: 1 ),
+                    ),
+                    SizedBox(
+                      height: size.height * .02,
+                    ),
+                    SizedBox(
+                      height: size.height * .09,
+                      width: size.width,
+                      child: TextFormField(
+                        focusNode: focusNode,
+                        controller: _controller,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                        decoration: InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.blue, width: 1.0),
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                          fillColor: Colors.grey,
+                          labelText: 'Phone, email address, or username',
+                          labelStyle: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          floatingLabelStyle: const TextStyle(
+                            color: Colors.blueAccent,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 }
